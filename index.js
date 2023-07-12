e4ce32c35c2a0f384c68cc5b68fccce9e2ac73fa
Daniel <danmcdonald95@outlook.com>
2023-07-11 23:12:07 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+cats = ["Milo", "Otis", "Garfield"];
+
+function cats(name) {}
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  return cats.shift(name);
+}
+
+function appendCat(name) {
+  return (name = [...cats, "Broom"]);
+}
+
+function prependCat(name) {
+  return (name = ["Arnold", ...cats]);
+}
+
+function removeLastCat(name) {
+  return (name = cats.slice(0, -1));
+}
+
+function removeFirstCat(name){
+
+    return (name =cats.slice(1));
+
+}
